952b8a000091c8b0c2ef5a5a501df354f107c1b8
alphacpc <alphacpc@gmail.com>
2022-01-02 12:10:58 +0000

    Upadte detail page

--- lib/screens/details.dart
+++ lib/screens/details.dart
@@ -8,20 +8,64 @@ class DetailScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    double fullWidthScreen = MediaQuery.of(context).size.width;
+    double fullHeightScreen = MediaQuery.of(context).size.height;
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Details [ ' + leader.leaderFname + ' ]'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            Text(leader.leaderLname),
-            Text(leader.leaderFname),
-            Text(leader.leaderMessage),
-          ],
+        // appBar: AppBar(
+        //   title: Text('Details [ ' + leader.leaderFname + ' ]'),
+        // ),
+        body: CustomScrollView(
+      slivers: [
+        SliverAppBar(
+          expandedHeight: fullHeightScreen * 0.4,
+          floating: false,
+          pinned: true,
+          flexibleSpace: FlexibleSpaceBar(
+            centerTitle: true,
+            title: Text('Details [ ' + leader.leaderFname + ' ]'),
+            background: Container(
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      colorFilter: new ColorFilter.mode(
+                          Colors.black.withOpacity(0.5), BlendMode.dstATop),
+                      image: new AssetImage(leader.leaderPicture),
+                      fit: BoxFit.cover)),
+            ),
+          ),
         ),
-      ),
-    );
+        SliverList(
+          delegate: SliverChildListDelegate([
+            Container(
+                child: Column(
+              children: [
+                Container(
+                  height: fullHeightScreen,
+                  width: fullWidthScreen,
+                  child: Column(
+                    children: [
+                      Text(leader.leaderLname),
+                      Text(leader.leaderFname),
+                      Text(leader.leaderMessage),
+                    ],
+                  ),
+                )
+              ],
+            )),
+          ]),
+        )
+      ],
+    ));
+
+    // Padding(
+    //   padding: const EdgeInsets.all(16.0),
+    //   child: Column(
+    //     children: [
+    //       Text(leader.leaderLname),
+    //       Text(leader.leaderFname),
+    //       Text(leader.leaderMessage),
+    //     ],
+    //   ),
+    // ),
   }
 }
